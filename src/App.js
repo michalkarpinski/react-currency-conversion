b727379db6d0f966972ce3adcb31db8811c9ab85
Michał <Micha>
2022-02-03 15:33:09 +0100

    base code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import CurrencyRow from "./CurrencyRow.js";
 
 function App() {
-  return;
+  return (
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </>
+  );
 }
 
 export default App;
